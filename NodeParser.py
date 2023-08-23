b8f67809a1b4ee58695eb9d32f28b057a3f3ffd5
olegogogo <oleg.ostapovich@bk.ru>
2023-08-23 10:18:24 +0300

    Finished merging

--- NodeParser.py
+++ NodeParser.py
@@ -1,14 +1,25 @@
-import dronecan, datetime, time
-from dronecan import uavcan
+#!/usr/bin/env python3
+"""
+Script used to listen nodes connected to CAN interface using NodeStatus protocol,
+summarising it and storing data to csv
+"""
 from argparse import ArgumentParser
+
+import datetime
+import time
 import pandas as pd
+import dronecan
+from dronecan import uavcan
 
 # get command line arguments
 parser = ArgumentParser(description='dump Node Status messages')
-parser.add_argument("--port", default='can0', type=str, help="serial port")
-parser.add_argument("--outfile", default='output', type=str, help="name of file where output will be stored")
-parser.add_argument("--logtime", default=3600, type=int, help="Logging duration in seconds"
-                                                              "3600 means the script will collect and summarise messages for one hour")
+parser.add_argument("--port", default='can0', type=str, help="CAN interface name")
+parser.add_argument("--outfile", default='output.csv', type=str,
+                    help="name of file where output will be stored with .csv extension")
+parser.add_argument("--logtime", default=3600, type=int,
+                    help="Logging duration in seconds"
+                         " 3600 means the script will collect"
+                         " and summarise messages for one hour")
 args = parser.parse_args()
 
 # Initializing a DroneCAN node instance.
@@ -18,7 +29,6 @@ node = dronecan.make_node(args.port, bitrate=1000000)
 node_monitor = dronecan.app.node_monitor.NodeMonitor(node)
 
 data = []
-finalDF = pd.DataFrame()
 columnnames = ['Date', 'Node id', 'Uptime', 'Health']
 
 
@@ -28,30 +38,36 @@ def handle_node_info(msg):
         :param msg:
         :return:
         '''
-    data.append(['{} {}'.format(datetime.date.today().strftime("%d/%m/%Y"),
-                                (datetime.datetime.now() + datetime.timedelta(hours=2)).strftime("%H:%M:%S")),
+    data.append([f'{datetime.date.today().strftime("%d/%m/%Y")}'
+                 f' {(datetime.datetime.now() + datetime.timedelta(hours=2)).strftime("%H:%M:%S")}',
                  msg.transfer.source_node_id,
                  datetime.timedelta(
                      seconds=int(str(dict(msg.transfer.payload._fields)['uptime_sec']))),
                  dict(msg.transfer.payload._fields)['health']])
 
 
+# Add a handler for the specified data type
 node.add_handler(uavcan.protocol.NodeStatus, handle_node_info)
 
 
 def spin_node():
+    """
+    spin node for specified logtime
+    :return:
+    """
     now = time.time()
     while now + args.logtime > time.time():
         try:
-            node.spin(timeout=args.logtime)  # spin node for hour and gathering dataset
+            node.spin(timeout=args.logtime)
 
-        except Exception as ex: # Print all errors if node gives broken message
+        except Exception as ex:  # Print all errors if node gives broken message
             print(ex)
 
 
 while True:
     try:
-        print('Listening nodes... : ', (datetime.datetime.now() + datetime.timedelta(hours=2)).strftime("%H:%M:%S"))
+        print('Listening nodes... : ',
+              (datetime.datetime.now() + datetime.timedelta(hours=2)).strftime("%H:%M:%S"))
         data = []
         spin_node()
         df = pd.DataFrame(data, columns=columnnames)
@@ -59,14 +75,13 @@ while True:
         listid = list(df['Node id'].unique())
 
         for i in listid:
-            finalDF = finalDF._append(pd.DataFrame(data=[[df['Date'][df['Node id']==i].iloc[-1],
-                                                          i,
-                                                          df['Uptime'][df['Node id']==i].iloc[-1],
-                                                          df['Health'][df['Node id']==i].iloc[-1]
-                                                          ]]
-                                                   )
-                                      )
-        # finally save to csv
-        finalDF.to_csv(args.outfile + '.csv', header=False, index=False)
+            # finally append each frame to csv
+            pd.DataFrame(data=[[df['Date'][df['Node id'] == i].iloc[-1],
+                                i,
+                                df['Uptime'][df['Node id'] == i].iloc[-1],
+                                df['Health'][df['Node id'] == i].iloc[-1]
+                                ]]
+                         ).to_csv(args.outfile, header=False, index=False, mode='a')
+
     except Exception:
         pass
